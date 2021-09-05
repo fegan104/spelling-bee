fdc789922df287ed9d24069cac788206c71ffca9
fegan104 <fegan104@gmail.com>
2021-09-04 20:10:05 -0400

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_kK3KrW0rxcfFowsfj2aRlXWMX46XlFU",
+  authDomain: "spelling-bee-365b3.firebaseapp.com",
+  projectId: "spelling-bee-365b3",
+  storageBucket: "spelling-bee-365b3.appspot.com",
+  messagingSenderId: "609204302314",
+  appId: "1:609204302314:web:cf7e5548ae19bab0818293",
+  measurementId: "G-YTVQKS7DKD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const sendToFirebaseAnalytics = ({ id, name, value }) => {
+  logEvent(analytics, name, {
+    category: 'Web Vitals',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+  });
+}
+
+export { sendToFirebaseAnalytics };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { fetchDictionary } from './dictionaryDataSource'
+import { sendToFirebaseAnalytics } from './firebase'
 
 //pre-fetch dicionary
 fetchDictionary()
@@ -18,4 +19,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//TODO automatically detect environment and use proerp reporting
+// reportWebVitals(console.log);
+reportWebVitals(sendToFirebaseAnalytics);
